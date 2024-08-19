8522b34f5bf4a3b19d89fd3311f6b2fde6d6de5d
Rikys <69305928+Rikys@users.noreply.github.com>
2024-08-19 17:37:42 +0200

    Fix install scrips spaces

--- Install/cdc-r.lua
+++ Install/cdc-r.lua
@@ -4,10 +4,10 @@ local files = {
     "machine/cdc.lua",
     "move.lua"
 }
-
+ 
 for _, file in ipairs(files) do
     shell.run("wget", "https://github.com/Rikys/Mekanism-x5-Plant-Manager/Turtle/" .. file)
 end
-
+ 
 -- Reboot turtle
 os.reboot()
