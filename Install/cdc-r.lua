dd8d07a7cdb86834ed4d7d89b7aaeb856403d005
Rikys <69305928+Rikys@users.noreply.github.com>
2024-08-19 17:34:58 +0200

    Add CDC-R installation script

---
+++ Install/cdc-r.lua
@@ -0,0 +1,13 @@
+-- Download necessary files
+local files = {
+    "startup/startup-cdc-r.lua",
+    "machine/cdc.lua",
+    "move.lua"
+}
+
+for _, file in ipairs(files) do
+    shell.run("wget", "https://github.com/Rikys/Mekanism-x5-Plant-Manager/Turtle/" .. file)
+end
+
+-- Reboot turtle
+os.reboot()
